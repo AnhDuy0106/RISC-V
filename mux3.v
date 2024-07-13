d2674548d296d2b1d3694d0a7627b87bfd0867c3
Bùi Nguyễn Anh Duy <126902854+AnhDuy0106@users.noreply.github.com>
2024-07-13 14:23:14 +0700

    Delete mux3.v

--- mux3.v
+++
@@ -1,9 +0,0 @@
-module mux3 (
-    input [31:0] a,
-    input [31:0] b,
-    input [31:0] c,
-    input [1:0] sel,
-    output [31:0] y
-);
-    assign y = (sel == 2'b00) ? a :(sel == 2'b01) ? b :c;
-endmodule
