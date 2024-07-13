27be5e49448c116b44612dd86a559b22fc780d02
Bùi Nguyễn Anh Duy <126902854+AnhDuy0106@users.noreply.github.com>
2024-07-13 14:27:08 +0700

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,59 +1,35 @@
 `timescale 1ns/1ns
-`include "top.v"
-`include "riscv_pip.v"
-`include "datapath_unit.v"
-`include "hazardunit.v"
-`include "controller.v"
-`include "maindec.v"
-`include "aludec.v"
-`include "alu.v"
-`include "imem.v"
-`include "regfile.v"
-`include "IEx_IMem.v"
-`include "IF_ID.v"
-`include "IMem_IW.v"
-`include "dmem.v"
-`include "ID_IEx.v"
-`include "mux2.v"
-`include "mux3.v"
-`include "extend.v"
-`include "flopenr.v"
-`include "adder.v"
-`include "c_ID_IEx.v"
-`include "c_IM_IW.v"
-`include "c_IEx_IM.v"
+`include "CPU.v"
+`include "RegisterFile.v"
+`include "InstructionMemory.v"
+`include "DataMemory.v"
+`include "ControlUnit.v"
+`include "ProgramCounter.v"
+`include "ALUControl.v"
+
 module testbench();
-    reg clock = 0;
+    reg clk = 0;
     reg reset;
-    
-    wire [31:0] writedata, dataAdr;
-    wire memwrite;
-    
-    top uut (
-        .clock(clock),
-        .reset(reset),
-        .writedataM(writedata),
-        .dataAdrM(dataAdr),
-        .memwriteM(memwrite)
+
+    CPU uut (
+        .clk(clk),
+        .reset(reset)
     );
 
+    always #10 clk = ~clk; 
+
     initial begin
-        reset <= 1;
-        #100;
-        reset <= 0;
-        $display("Start");
+        reset = 1;
+        #100
+        reset = 0; 
+      	$display("Start");
         #1000; 
-        $display("Value of ALU = %d", dataAdr);
+        $display("Value of ALU = %h", uut.ALU.Result);
         $finish;
-    end
 
-    always #10 clock <= ~clock;
-
-    initial begin
-        $dumpfile("dump.vcd");
-        $dumpvars(0);
     end
+  initial begin
+    $dumpfile ("dump.vcd");
+    $dumpvars(0);
+  end
 endmodule
-
-		
-
